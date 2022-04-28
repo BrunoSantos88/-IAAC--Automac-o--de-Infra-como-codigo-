6557f3b037530b95f460017aaae0e71ebef569af
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-04-28 11:27:34 -0300

    Update ec2-aws.py

--- Boto3-python/INSTANCIA/ec2-aws.py
+++ Boto3-python/INSTANCIA/ec2-aws.py
@@ -1,8 +1,8 @@
 import boto3 
 ec2 = boto3.client('ec2', 
                    'us-east-1', 
-                   aws_access_key_id='AKIATX4TOZZAD77AB3ML', 
-                   aws_secret_access_key='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') 
+                   aws_access_key_id='########', 
+                   aws_secret_access_key='############') 
 conn = ec2.run_instances(InstanceType="t2.micro", 
                          MaxCount=1, 
                          MinCount=1, 
